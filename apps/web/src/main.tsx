4105387149941e542f27fc283b0d639d303e5615
Marcel Tabara <marcel.tabara@gmail.com>
2022-12-03 13:02:26 -0500

    fix: rename apps

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,16 +1,18 @@
+import { store } from '@myapp/store';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import App from './app/App';
+import { App } from './app/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
