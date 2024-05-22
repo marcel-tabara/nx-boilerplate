a7b434a7e7774827424b8511be7ed7f9b862bd64
Marcel Tabara <marcel.tabara@gmail.com>
2024-05-22 14:32:36 -0400

    update rtk fetch hooks

--- apps/nxb-react/src/app/App.tsx
+++ apps/nxb-react/src/app/App.tsx
@@ -1,26 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 import { NxbComponentsButton } from '@nxb-components-react';
-import { externalApi, internalApi } from '@nxb-services';
-import { skipToken } from '@reduxjs/toolkit/query/react';
-import { useState } from 'react';
+import { externalApi } from '@nxb-services';
 
 export const App = () => {
-  const [skip, setSkip] = useState(true);
   const {
     data: dataExternal,
     error: errorExternal,
     isLoading: isLoadingExternal,
     refetch: refetchExternal,
-  } = externalApi.useFetchAllEntriesQuery(skipToken);
-
-  const {
-    data: dataInternal,
-    error: errorInternal,
-    isLoading,
-    isLoading: Internal,
-    refetch: refetchInternal,
-  } = internalApi.useFetchAllEntriesQuery();
+  } = externalApi.useFetchAllEntriesQuery();
 
   const [trigger, result, lastPromiseInfo] =
     externalApi.useLazyFetchAllEntriesQuery();
@@ -28,21 +17,15 @@ export const App = () => {
   return (
     <>
       <div>Web App</div>
-      {/* <div>
-        Data:
-        <span>{JSON.stringify(dataExternal)}</span>
-      </div>
+      {
+        <div>
+          Data:
+          <span>{JSON.stringify(dataExternal)}</span>
+        </div>
+      }
       <div>
-        Result:
-        <span>{JSON.stringify(result)}</span>
-      </div>
-      <div>
-        lastPromiseInfo:
-        <span>{JSON.stringify(lastPromiseInfo)}</span>
-      </div> */}
-      <div>
-        <NxbComponentsButton onClick={trigger} title="Trigger" />
-        <NxbComponentsButton onClick={refetchExternal} title="Refetch" />
+        {<NxbComponentsButton onClick={refetchExternal} title="Refetch" />}
+        <NxbComponentsButton onClick={() => trigger()} title="Trigger lazy" />
       </div>
     </>
   );
